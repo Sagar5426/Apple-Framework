654fef52889d57a9105f6f5e8debe376d8123a25
Sagar5426 <108585817+Sagar5426@users.noreply.github.com>
2024-05-17 15:20:16 +0530

    Initial Commit

---
+++ Apple Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple Framework
+//
+//  Created by Sagar Jangra on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
